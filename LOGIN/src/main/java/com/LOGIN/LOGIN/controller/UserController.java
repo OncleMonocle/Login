81bc6f3cc17386773a2c0f1549c102ba8723ca6c
ErikLtz1 <eriklilliecreutz@gmail.com>
2024-01-08 12:04:28 +0100

    Vid skapning av användare så krypteras lösenordet och sparas i databasen (Fix #19)

--- LOGIN/src/main/java/com/LOGIN/LOGIN/controller/UserController.java
+++ LOGIN/src/main/java/com/LOGIN/LOGIN/controller/UserController.java
@@ -3,6 +3,9 @@ package com.LOGIN.LOGIN.controller;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,6 +23,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @CrossOrigin
 public class UserController {
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
     
     private final UserRepository userRepository;
 
@@ -34,7 +40,7 @@ public class UserController {
     @GetMapping ("/")
     public String index (Model model) {
         model.addAttribute("productList", productRepository.findAll());
-        System.out.println(userRepository.findById(1).get().getFirstName());
+        // System.out.println(userRepository.findById(5).get().getFirstName());
         return "index.html";
     }
 
@@ -67,10 +73,14 @@ public class UserController {
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setEmail(email);
-        user.setPassword(password);
+        String encryptedPassword = bcryptEncoder.encode(password);
+        user.setPassword(encryptedPassword);
         userRepository.save(user);
+
+        
 
 		return "redirect:/";
 	}
+    
     
 }
